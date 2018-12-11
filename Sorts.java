9a06cef49ad13044048238b6da853940a12d8d6c
Sublime <other@Alberts-MBP.fios-router.home>
2018-12-10 20:20:13 -0500

    finished bubble sort

--- Sorts.java
+++ Sorts.java
@@ -14,20 +14,17 @@ public class Sorts{
       ary[smallest] = first;
     }
   }
-  public static void bubbleSort(int[] data){  
-		boolean switched;
-		int numswitch = 0;
+  public static void bubbleSort(int[] data){
+		boolean switched = true;
 		while (switched){
+      switched = false;
 			for (int x =0; x< data.length - 1;x++){
 				if (data[x] > data[x + 1]){
-					int old = data[x + 1];
-					data[x + 1] = data[x];
-					data[x] = old;
-					numswitch ++;
+					int old = data[x];
+					data[x] = data[x + 1];
+					data[x + 1] = old;
+          switched = true;
 				}
-			if (numswitch == 0){
-				switched = false;
-			}
 			}
 		}
 	}
@@ -38,7 +35,7 @@ public class Sorts{
     for (int x = 0; x < arr.length;x++){
       System.out.println(arr[x]);
     }
-    int[] arr2 = {-12321312, 2, 3, 3, 4, 4, 8, 12};
+    int[] arr2 = {-12321312, 2, 3, 3, 4, 4, 8, 12, 3,3,4,3,4,34,0,6,2,45,2,45,234,5,23,45};
     bubbleSort(arr2);
     for (int x = 0; x < arr2.length; x++){
     	System.out.println(arr2[x]);
