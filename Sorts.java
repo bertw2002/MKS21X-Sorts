48370b9d1a712fe9eef7a213343662af65a213e8
Sublime <other@Alberts-MBP.fios-router.home>
2018-12-10 21:24:15 -0500

    made insertion code

--- Sorts.java
+++ Sorts.java
@@ -28,6 +28,30 @@ public class Sorts{
 			}
 		}
 	}
+  public static void insertionSort(int[] ary){
+    int least = 0;
+    int secondi = 0;
+    /*
+    a-Find the smallest element located at index i or any of the higher indices.
+    b-Swap that value into index i.
+    c-Increase i
+    */
+    //loop to repeat steps a, b, and c.
+    for (int x = 0; x < ary.length; x++){
+      least = ary[x];
+      //finds smallest from index x
+      for (int y = x; y < ary.length; y ++){
+        if (least > ary[y]){
+          least = ary[y];
+          secondi = y;
+        }
+      }
+      //sets values
+      int old = ary[x];
+      ary[x] = least;
+      ary[secondi] = old;
+    }
+  }
 
   public static void main(String[] args){
     int[] arr = {-1231231, 2, 3, 3, 4, 2, 4, 8, 12};
@@ -39,6 +63,11 @@ public class Sorts{
     bubbleSort(arr2);
     for (int x = 0; x < arr2.length; x++){
     	System.out.println(arr2[x]);
+    }
+    int[] arr3 = {-12321312, 2, 3, 3, 4, 4, 8, 12, 3,3,4,3,4,34,0,6,2,45,2,45,234,5,23,45};
+    insertionSort(arr3);
+    for (int x = 0; x < arr3.length; x++){
+      System.out.println(arr3[x]);
     }
   }
 
