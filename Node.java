0c7f5b06104f049a027dde9b1b0e1360dcd871be
Sublime <other@Alberts-MBP.fios-router.home>
2018-12-12 18:46:25 -0500

    contructor get set

--- Node.java
+++ Node.java
@@ -1,1 +1,27 @@
-
+public class Node{
+  private int data;
+  private Node next,prev;
+  public Node(int data, Node next, Node prev){
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+  public int getData(){
+    return data;
+  }
+  public Node getNext(){
+    return next;
+  }
+  public Node getPrev(){
+    return prev;
+  }
+  public void setData(){
+    this.data = data;
+  }
+  public void setNext(){
+    this.next = next;
+  }
+  public void setPrev(){
+    this.prev = prev;
+  }
+}
